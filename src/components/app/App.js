99d1ba90e2cdb6797a1ef30202e26c67ce3c97d8
Aaron Brooks <aaron@Aarons-MBP-2.attlocal.net>
2021-12-27 19:20:14 -0500

    getting component structure and index.module.css

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,11 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import styles from './index.modules.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Aaron Brooks
         </p>
--- src/components/app/App.test.js
+++ src/components/app/App.test.js
@@ -2,6 +2,6 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-  const {container} = render(<App />);
+  const {container} = render(<App/>);
   expect(container).toMatchSnapshot();
 });
