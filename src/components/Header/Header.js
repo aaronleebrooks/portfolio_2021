5496cb6713d389ed0a93929709c40c2f5eb518cd
Aaron Brooks <aaron@Aarons-MBP-2.attlocal.net>
2021-12-27 22:17:54 -0500

    getting header styling

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,18 +1,40 @@
 import Button from "../Button";
+import styles from "./index.module.css";
+import cx from "classnames";
+
+const theme = {
+  root: cx(
+    styles.headerBar
+  ),
+  navigation: cx(
+    styles.navSection
+  ),
+  logo: cx(
+    styles.headerLogo,
+    styles.headerText,
+    styles.largeHeaderText
+  ),
+  linkButton: cx(
+    styles.headerText,
+    styles.smallHeaderText
+  ),
+}
 
 function Header() {
   return (
-    <div>
-      <span>Aaron Brooks</span>
-      <Button className="resume">
-        Resume
-      </Button>
-      <Button>
-        Demos
-      </Button>
-      <Button>
-        Contact Me
-      </Button>
+    <div className={theme.root}>
+      <span className={theme.logo}>Aaron Brooks</span>
+      <span className={theme.navigation}>
+        <Button className={theme.linkButton}>
+          Resume
+        </Button>
+        <Button className={theme.linkButton}>
+          Demos
+        </Button>
+        <Button className={theme.linkButton}>
+          Contact Me
+        </Button>
+      </span>
     </div>
   );
 }
