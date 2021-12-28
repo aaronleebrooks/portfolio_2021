f85e58d178a1420454f39d5358ca86890f73b42b
Aaron Brooks <aaron@Aarons-MBP-2.attlocal.net>
2021-12-27 20:09:59 -0500

    getting rid of create react app stuff

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,25 +1,7 @@
-import styles from './index.modules.css';
-import cx from 'classnames';
-
 function App() {
   return (
-    <div className={styles.App}>
-      <header className="App-header">
-        <p>
-          Aaron Brooks
-        </p>
-        <p>
-          Software Engineer
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      
     </div>
   );
 }
